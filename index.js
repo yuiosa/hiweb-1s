f33d7c0a55d4cb0d5527af15e3d423f9c68acd80
Heroku-v2rays <94675428+yuiosa@users.noreply.github.com>
2023-01-09 13:01:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import fs from "fs"
 
 const httpServer = http.createServer();
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const bareServer = createBareServer('/bare/', {});
 
 // #############################################################################
